642de8e6b7bef53126ce11fb5e8597f8a167a58f
Vladimir Kravtsov <vladhc@gmail.com>
2021-08-23 19:55:12 +0200

    Better foler layout

--- build.sh
+++ build.sh
@@ -20,34 +20,35 @@ do
   shift
 done
 
-SRC_COMMON="geom.cc tank.cc env.cc  strategic_point.cc"
+mkdir -p build
+SRC_COMMON="src/geom.cc src/tank.cc src/env.cc src/strategic_point.cc"
 INCLUDE_COMMON="-Iextern/box2d/include"
 
 if $BUILD_TESTBED
 then
-  rm -f render
+  rm -f build/render
   echo "Building Testbed"
   c++ $SRC_COMMON \
-    keyboard_controller.cc \
-    render.cc \
+    src/keyboard_controller.cc \
+    src/render.cc \
     -g -rdynamic \
     $INCLUDE_COMMON \
     -std=c++11 -w \
     -Llib \
     -lSDL2 \
     -lbox2d \
-    -o render
+    -o build/render
   echo "Done"
 fi
 
 if $BUILD_PYMODULE
 then
-  FILENAME="tanks$(python-config --extension-suffix)"
+  FILENAME="build/tanks$(python-config --extension-suffix)"
   rm -f $FILENAME
   echo "Building python shared library"
   c++ \
     $SRC_COMMON \
-    module.cc \
+    src/module.cc \
     -O3 -Wall -shared -std=c++11 \
     -fPIC \
     $(python-config --includes) \
--- run-tests.sh
+++ run-tests.sh
@@ -2,16 +2,18 @@
 
 set -e  # exit when any command fails
 
+mkdir -p build
+FILENAME="build/env-test"
+rm -f "$FILENAME"
 c++ \
   -Wall -std=c++11 \
-  -o env-test \
+  -o "$FILENAME" \
   -Iextern/googletest/googletest/include \
   -Iextern/box2d/include \
   -Llib \
-  -Lextern/box2d/build/bin \
   -lgtest \
   -lbox2d \
   -pthread \
-  *_test.cc env.cc tank.cc geom.cc
+  src/*_test.cc src/env.cc src/tank.cc src/geom.cc
 
-LD_LIBRARY_PATH="$(pwd)/lib:$LD_LIBRARY_PATH" ./env-test
+LD_LIBRARY_PATH="$(pwd)/lib:$LD_LIBRARY_PATH" $FILENAME
